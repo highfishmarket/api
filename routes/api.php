98b01028ec7ac762b1e50aa471aebea8fe35a767
yunpen06 <yunpen06@limefriends.com>
2023-04-03 17:16:40 +0900

    [feature] api route 작성

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Models\Article;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['prefix'=> 'articles'], function (){
+    Route::get('/', function (){
+        return Article::all();
+    });
+    Route::get('/{id}', function ($id){
+        return Article::find($id);
+    });
+    Route::post('/', function (Request $request){
+        return Article::create($request->all());
+    });
+    Route::put('/{id}', function (Request $request, $id){
+        $article = Article::findOrFail($id);
+        $article->update($request->all());
+        return Article::all();
+    });
+    Route::delete('/{id}', function ($id){
+        Article::find($id)->delete();
+        return 204;
+    });
+});
+
